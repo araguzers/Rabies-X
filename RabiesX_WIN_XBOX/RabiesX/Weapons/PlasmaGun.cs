aaa1209f295ff56836368b041c9fd610d0521224
araguzcommander <miminjr87@gmail.com>
2011-10-22 15:22:13 -0500

    Committing weapon classes to git.

---
+++ RabiesX_WIN_XBOX/RabiesX/Weapons/PlasmaGun.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RabiesX
+{
+    public class PlasmaGun
+    {
+        public int MaximumPlasma { get; set; }
+        public int Plasma { get; set; }
+        public bool Empty { get; set; }
+
+        public PlasmaGun(int maximum)
+        {
+            MaximumPlasma = maximum;
+            Plasma = MaximumPlasma;
+            Empty = false;
+        }
+
+        public void Boost(int boost)
+        {
+            MaximumPlasma += boost;
+        }
+
+        public void Recharge()
+        {
+            Plasma = MaximumPlasma;
+        }
+
+        public void Waste(int plasma)
+        {
+            Plasma -= plasma;
+            if (Plasma == 0)
+                Empty = true;
+        }
+    }
+}
---
+++ RabiesX_WIN_XBOX/RabiesX/Weapons/Sword.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RabiesX
+{
+    public class Sword
+    {
+        public int MaximumDurability {get; set;}
+        public int Durability { get; set; }
+        public bool Broken { get; set; }
+
+        public Sword(int maximum)
+        {
+            MaximumDurability = maximum;
+            Durability = MaximumDurability;
+            Broken = false;
+        }
+
+        public void Boost(int boost)
+        {
+            MaximumDurability += boost;
+        }
+
+        public void Repair()
+        {
+            Durability = MaximumDurability;
+        }
+
+        public void WearOut(int damage)
+        {
+            Durability -= damage;
+            if (Durability == 0)
+                Broken = true;
+        }
+    }
+}
