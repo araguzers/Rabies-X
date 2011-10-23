4ef29f18170005c91b16fafe46bdcf9daf809f53
araguzcommander <miminjr87@gmail.com>
2011-10-23 18:10:58 -0500

    Added a couple of features to the enemy class. Enemies have 2 extra attributes:
    speed, which will increase over time
    type, which represents the category: human, dog or bird.

--- RabiesX_WIN_XBOX/RabiesX/Characters/Enemy.cs
+++ RabiesX_WIN_XBOX/RabiesX/Characters/Enemy.cs
@@ -5,8 +5,23 @@ using System.Text;
 
 namespace RabiesX
 {
-    public class Enemy
+    public class Enemy : Character
     {
+        public int Speed { get; set; }
+        public string Type { get; set; }
 
+        public Enemy() : base() {}
+
+        public Enemy(int startHealth, int startDefense, int startHitsToWound, int startStrength, int startSpeed, string type)
+            : base(startHealth, startDefense, startHitsToWound, startStrength)
+        {
+            Type = type;
+            Speed = startSpeed;
+        }
+
+        public void Quicken(int speed)
+        {
+            Speed += speed;
+        }
     }
 }
