a8ca1c858230ae57e0cf87cb594cddecef3af276
araguzcommander <miminjr87@gmail.com>
2011-10-29 23:35:15 -0500

    Added more functions to the storage module, with the help of the Microsoft website. The file IO is now all here.

--- RabiesX_WIN_XBOX/RabiesX/Storage/Storage.cs
+++ RabiesX_WIN_XBOX/RabiesX/Storage/Storage.cs
@@ -17,6 +17,13 @@ namespace RabiesX
 {
     public class Storage
     {
+        public bool MetJackson { get; set; }
+
+        public Storage()
+        {
+            MetJackson = false;
+        }
+
         public void CreateStorage(StorageDevice device, string storagename, string savename)
         {
             IAsyncResult result = device.BeginOpenContainer(storagename, null, null);
@@ -42,7 +49,99 @@ namespace RabiesX
             container.Dispose();
         }
 
-        public void SaveGame(StorageDevice device, string storagename, string savename, Character character)
+        public void Load(StorageDevice device, string storagename, string loadname, Character character)
+        {
+            IAsyncResult result = device.BeginOpenContainer(storagename, null, null);
+            result.AsyncWaitHandle.WaitOne();
+            StorageContainer container = device.EndOpenContainer(result);
+            result.AsyncWaitHandle.Close();
+            string line;
+            string[] words;
+            Stream stream = container.OpenFile(loadname, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+            if (character is Protagonist)
+            {
+                while (!reader.EndOfStream)
+                {
+                    line = reader.ReadLine();
+                    words = line.Split(' ');
+                    if (words[0] == "Health")
+                        character.Health = Convert.ToInt16(words[1]);
+                    else if (words[0] == "Defense")
+                        character.Defense = Convert.ToInt16(words[1]);
+                    else if (words[0] == "Attack")
+                        character.Attack = Convert.ToInt16(words[1]);
+                    else if (words[0] == "HitsToWound")
+                        character.HitsToWound = Convert.ToInt16(words[1]);
+                    else if (words[0] == "Alive")
+                    {
+                        if (words[1] == "yes")
+                            character.Alive = true;
+                        else
+                            character.Alive = false;
+                    }
+                    else if (words[0] == "MaximumPlasma")
+                        ((Protagonist)character).plasmaGun.MaximumPlasma = Convert.ToInt16(words[1]);
+                    else if (words[0] == "Plasma")
+                        ((Protagonist)character).plasmaGun.Plasma = Convert.ToInt16(words[1]);
+                    else if (words[0] == "Empty")
+                    {
+                        if (words[1] == "no")
+                            ((Protagonist)character).plasmaGun.Empty = false;
+                        else
+                            ((Protagonist)character).plasmaGun.Empty = true;
+                    }
+                }
+            }
+            else if (character is Antagonist)
+            {
+                while (!reader.EndOfStream)
+                {
+                    line = reader.ReadLine();
+                    words = line.Split(' ');
+                    if (words[0] == "Met")
+                    {
+                        if (words[1] == "no")
+                        {
+                            MetJackson = false;
+                            break;
+                        }
+                        MetJackson = true;
+                    }
+                    else if (words[0] == "Health")
+                        character.Health = Convert.ToInt16(words[1]);
+                    else if (words[0] == "Defense")
+                        character.Defense = Convert.ToInt16(words[1]);
+                    else if (words[0] == "Attack")
+                        character.Attack = Convert.ToInt16(words[1]);
+                    else if (words[0] == "HitsToWound")
+                        character.HitsToWound = Convert.ToInt16(words[1]);
+                    else if (words[0] == "Alive")
+                    {
+                        if (words[1] == "yes")
+                            character.Alive = true;
+                        else
+                            character.Alive = false;
+                    }
+                    else if (words[0] == "MaximumDurability")
+                        ((Antagonist)character).sword.MaximumDurability = Convert.ToInt16(words[1]);
+                    else if (words[0] == "Durability")
+                        ((Antagonist)character).sword.Durability = Convert.ToInt16(words[1]);
+                    else if (words[0] == "Broken")
+                    {
+                        if (words[1] == "no")
+                            ((Antagonist)character).sword.Broken = false;
+                        else
+                            ((Antagonist)character).sword.Broken = true;
+                    }
+                }
+            }
+            reader.Close();
+            stream.Close();
+            container.Dispose();
+        }
+
+        public void Save(StorageDevice device, string storagename, string savename, Character character)
         {
             IAsyncResult result = device.BeginOpenContainer(storagename, null, null);
             result.AsyncWaitHandle.WaitOne();
@@ -71,5 +170,65 @@ namespace RabiesX
             stream.Close();
             container.Dispose();
         }
+
+        public void Rename(StorageDevice device, string storagename, string oldsavename, string newsavename)
+        {
+            IAsyncResult result = device.BeginOpenContainer(storagename, null, null);
+            result.AsyncWaitHandle.WaitOne();
+            StorageContainer container = device.EndOpenContainer(result);
+            result.AsyncWaitHandle.Close();
+            if (container.FileExists(oldsavename) && !container.FileExists(newsavename))
+            {
+                Stream oldStream = container.OpenFile(oldsavename, FileMode.Open);
+                Stream newStream = container.CreateFile(newsavename);
+                oldStream.CopyTo(newStream);
+                oldStream.Close();
+                newStream.Close();
+                container.DeleteFile(oldsavename);
+            }
+            container.Dispose();
+        }
+
+        public void Copy(StorageDevice device, string storagename, string savename, string copyname)
+        {
+            IAsyncResult result = device.BeginOpenContainer(storagename, null, null);            
+            result.AsyncWaitHandle.WaitOne();
+            StorageContainer container = device.EndOpenContainer(result);
+            result.AsyncWaitHandle.Close();
+            if (container.FileExists(savename) && !container.FileExists(copyname))
+            {
+                Stream stream = container.OpenFile(savename, FileMode.Open);
+                Stream copyStream = container.CreateFile(copyname);
+                stream.CopyTo(copyStream);
+                stream.Close();
+                copyStream.Close();
+            }
+            container.Dispose();
+        }
+
+        private static void Display(StorageDevice device, string storagename)
+        {
+            IAsyncResult result = device.BeginOpenContainer(storagename, null, null);
+            result.AsyncWaitHandle.WaitOne();
+            StorageContainer container = device.EndOpenContainer(result);
+            result.AsyncWaitHandle.Close();
+            string[] savedGames = container.GetFileNames();
+            foreach (string game in savedGames)
+                Console.WriteLine(savedGames);
+            container.Dispose();
+        }
+
+
+        private static void Delete(StorageDevice device, string storagename, string deletename)
+        {
+            IAsyncResult result = device.BeginOpenContainer(storagename, null, null);            
+            result.AsyncWaitHandle.WaitOne();
+            StorageContainer container = device.EndOpenContainer(result);
+            result.AsyncWaitHandle.Close();
+            if (container.FileExists(deletename))
+                container.DeleteFile(deletename);
+            container.Dispose();
+        }
+
     }
 }
