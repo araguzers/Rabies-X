16c5e9e3a3402dac6d6da2632158ba2a1b1e1f9b
araguzcommander <miminjr87@gmail.com>
2011-10-30 21:25:34 -0500

    Added a little class for the collectible, with the attributes being the type of collectible, the character that the collectible goes to, and the amount of "effect" it provides.

---
+++ RabiesX_WIN_XBOX/RabiesX/Collectibles/Collectible.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RabiesX
+{
+    public class Collectible
+    {
+        public bool TargetCharacter { get; set; } //true if Araguz, false if Jackson
+        public string Type { get; set; } //represents the type of collectible, e.g. "plasma container", "hammer", etc.
+        public int Effect { get; set; }
+
+        public Collectible() { }
+
+        public Collectible(string character, string type, int howMuch)
+        {
+            if (character == "araguz")
+                TargetCharacter = true;
+            else
+                TargetCharacter = false;
+            Type = type;
+            Effect = howMuch;
+        }
+
+        public void GetCollected()
+        {
+            Effect = 0;
+        }
+    }
+}
